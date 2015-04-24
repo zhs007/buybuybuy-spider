bee3adc00b4834a8b481aa61a6e5418418379e12
zhs007 <sssxueren@gmail.com>
2015-04-24 18:35:24 +0800

    数据层框架搭建

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zhs007 on 15/4/24.
+ */
+
+// 数据管理器类型
+exports.datamgr_type = 'mysql';
+
+// mysql配置
+exports.mysql_host = '121.41.86.183';
+exports.mysql_user = 'zhs007';
+exports.mysql_pwd = 'zhs007';
+exports.mysql_name = 'buybuybuy';
+
